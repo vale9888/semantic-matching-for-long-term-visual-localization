80a11dfb13ae5d5e2f9eddecb0c4374bd92a57e3
Valentina Sgarbossa <65012675+vale9888@users.noreply.github.com>
2022-11-06 17:01:16 +0100

    reference FGSN

---
+++ fine_grained_segmentation/eval/context.py
@@ -0,0 +1,6 @@
+# Script originally from https://github.com/maunzzz/fine-grained-segmentation-networks,
+# licensed as in the LICENSE file of the above repository (Attribution-NonCommercial 4.0 International).
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
